3dad706ff89df542317bfc189d3491b53eeff002
KatoTimothy <katotimuci@gmail.com>
2023-12-31 21:46:36 +0300

    set up the app

---
+++ app/dashboard/bookables/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BookablesPage() {
+  return (
+    <div>This is a bookables page</div>
+  )
+}
+
+export default BookablesPage
---
+++ app/dashboard/bookings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BookingsPage() {
+  return (
+    <div>This is a bookings page</div>
+  )
+}
+
+export default BookingsPage
