5fef73e5f45cc71573e016f79aee186ef77ce324
KatoTimothy <katotimuci@gmail.com>
2024-02-09 11:52:31 +0300

    Create page.tsx
    Page component that displays content of a bookable when the dynamic url of the form '/bookables/[id]' matches

---
+++ app/bookables/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React, { Suspense } from "react";
+import BookablesList from "../bookables-list";
+import { findBookableById } from "@/app/lib/api";
+import BookableDetails from "../bookable-details";
+
+async function BookableDetailsPage({ params }: { params: { id: number } }) {
+  const id = params.id || 1;
+  const bookable = await findBookableById(id);
+
+  return <BookableDetails bookable={bookable} />;
+}
+
+export default BookableDetailsPage;
