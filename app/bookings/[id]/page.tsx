c118f876ff3c40d8942adda8e934d6943f55e3b4
KatoTimothy <katotimuci@gmail.com>
2024-02-14 14:56:25 +0300

    Create page.tsx

---
+++ app/bookings/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React, { Suspense } from "react";
+import WeekPicker from "../week-picker";
+import BookingsPageWrapper from "../bookings-page-wrapper";
+
+async function BookingsHomePage({}) {
+  return (
+    <div className="bookings-grid">
+      <WeekPicker />
+      <BookingsPageWrapper />
+    </div>
+  );
+}
+
+export default BookingsHomePage;
