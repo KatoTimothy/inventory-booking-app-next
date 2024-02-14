be32c0d5714a140c8f5f3d058547b1b32942b4f5
KatoTimothy <katotimuci@gmail.com>
2024-02-14 14:56:23 +0300

    Update bookables-list.tsx

--- app/bookings/bookables-list.tsx
+++ app/bookings/bookables-list.tsx
@@ -1,51 +1,40 @@
 "use client";
 
 import { Bookable } from "../lib/types-definitions";
-import {
-  useRouter,
-  useParams,
-  useSearchParams,
-  redirect,
-} from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 
 import clsx from "clsx";
 
 function BookablesList({ bookables }: { bookables: Bookable[] }) {
+  // Read param
+  const params = useParams();
+  const bookableId = Number(params.id || bookables[0].id);
+
   const router = useRouter();
 
-  const params_ = useSearchParams();
-  const params = new URLSearchParams(params_);
-
-  const id = Number(params.get("bookableId")) || 1;
-
-  const groups = Array.from(new Set(bookables.map((b) => b.group)));
-  const bookable = bookables.find((b) => b.id == id);
+  // bookable state
+  const bookable = bookables.find((b) => b.id == bookableId);
   const group = bookable?.group;
-
-  //State values
-  // const [group, setGroup] = useState(groups[0]);
-  // const [bookableIndex, setBookableIndex] = useState(0);
-
   const bookablesInGroup = bookables.filter((b) => b.group === group);
 
-  function handleChangeGroup(e: React.ChangeEvent<HTMLSelectElement>) {
-    // setGroup(e.target.value);
-    // setBookableIndex(0);
+  // All unique group names
+  const groups = Array.from(new Set(bookables.map((b) => b.group)));
 
+  // Event handlers
+  function handleChangeGroup(e: React.ChangeEvent<HTMLSelectElement>) {
     const bookablesInSelectedGroup = bookables.filter(
       (b) => b.group === e.target.value
     );
-    router.push(`/bookings?bookableId=${bookablesInSelectedGroup[0].id}`);
+
+    router.replace(`/bookings/${bookablesInSelectedGroup[0].id}`);
   }
 
   function handleChangeBookable(id: number) {
-    // setBookableIndex(id);
-    params.set("bookableId", id.toString());
-    router.push(`/bookings?${params.toString()}`);
-    // redirect(`/bookings?bookableId=${id}`);
+    router.replace(`/bookings/${id}`);
   }
 
   return (
+    // <div className="">
     <div className="flex flex-col items-center">
       <div className="group-selector">
         <select
@@ -53,21 +42,21 @@ function BookablesList({ bookables }: { bookables: Bookable[] }) {
           id="group"
           className={`py-1 px-2 outline border-none outline-1 rounded-sm bg-white`}
           onChange={(e) => handleChangeGroup(e)}
-          value={group}
+          defaultValue={group}
         >
           {groups.map((g, i) => (
             <option key={i}>{g}</option>
           ))}
         </select>
       </div>
-      <ul className="mt-6 w-full flex flex-col gap-2 bookables-list">
+      <ul className="mt-6 pb-2 w-full flex flex-row max-sm:overflow-x-auto sm:flex-col whitespace-nowrap gap-2 bookables-list">
         {bookablesInGroup.map((b, i) => {
           return (
             <li
               className={`${clsx(
-                "text-center rounded-full px-2 py-1 border border-gray-600",
-                { "bg-accent-800 text-white": b.id === id },
-                { "text-gray-700 bg-white": b.id !== id }
+                "text-center rounded-full px-2 py-1 border border-gray-600 cursor-pointer",
+                { "bg-accent-800 text-white": b.id === bookableId },
+                { "text-gray-700 bg-white": b.id !== bookableId }
               )}`}
               key={i}
               onClick={() => handleChangeBookable(b.id)}
@@ -78,6 +67,7 @@ function BookablesList({ bookables }: { bookables: Bookable[] }) {
         })}
       </ul>
     </div>
+    // </div>
   );
 }
 
