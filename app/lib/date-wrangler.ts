f3ddafea5dd499552ddcd7fc86f506ac7819fff8
KatoTimothy <katotimuci@gmail.com>
2024-02-19 20:53:12 +0300

    Update date-wrangler.ts
    Fixed a bug that made the getWeek() function

--- app/lib/date-wrangler.ts
+++ app/lib/date-wrangler.ts
@@ -1,15 +1,14 @@
 //offsets given date by number of days
-export function offSetDate(date: string, offset: number) {
+export function offSetDate(date: string | Date, offset: number) {
   const dateClone = new Date(date);
   dateClone.setDate(dateClone.getDate() + offset);
   return dateClone;
 }
 
 //gets start and end dates of particular week given an arbitrary date
-export function getWeek(date: string, offset: number = 0) {
-  const day = offSetDate(date, offset).getDay();
-  const targetDate = offSetDate(date, 0);
-  const startDate = offSetDate(date, -day);
+export function getWeek(date: Date, offset: number = 0) {
+  const targetDate = offSetDate(date, offset);
+  const startDate = offSetDate(date, -targetDate.getDay());
   const endDate = offSetDate(date, 6 - targetDate.getDay());
   return { targetDate, startDate, endDate };
 }
@@ -18,4 +17,3 @@ export function getWeek(date: string, offset: number = 0) {
 export function shortISODate(date: Date) {
   return date.toISOString().split("T")[0];
 }
-
