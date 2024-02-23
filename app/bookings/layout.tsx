e8c78f45b172c72acda1a60f3afc00b617f4ca65
KatoTimothy <katotimuci@gmail.com>
2024-02-23 09:48:06 +0300

    Update layout.tsx
    Included WeekPicker component in the layout page for /bookings route

--- app/bookings/layout.tsx
+++ app/bookings/layout.tsx
@@ -1,20 +1,19 @@
-import React, { Suspense } from "react";
-import BookingsGrid from "./bookings-grid";
-import { getAllBookables, getAllBookings } from "../lib/api";
+import React from "react";
+import { getAllBookables } from "../lib/api";
 import BookablesList from "./bookables-list";
-import PageLoader from "../loading";
-import BookingsPageWrapper from "./bookings-page-wrapper";
+import WeekPicker from "./week-picker";
 
-async function BookingsLayoutPage({ children }: { children: React.ReactNode }) {
+async function BookingsPage({ children }: { children: React.ReactNode }) {
   const bookables = await getAllBookables();
-  // const bookings = await getAllBookings();
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-[minmax(0,1fr),minmax(0,3fr)] gap-8 justify-between mt-10">
       <BookablesList bookables={bookables} />
-      {children}
+      <div className="bookings-section">
+        <WeekPicker />
+        {children}
+      </div>
     </div>
   );
 }
 
-export default BookingsLayoutPage;
+export default BookingsPage;
