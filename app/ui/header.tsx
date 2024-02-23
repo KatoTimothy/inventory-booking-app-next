0a48c81b7062905f9a47728832ee24d13f5523c8
KatoTimothy <katotimuci@gmail.com>
2024-02-23 10:02:16 +0300

    Update header.tsx

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -4,7 +4,7 @@ import { NavLinks } from "./nav-links";
 
 const Header = () => {
   return (
-    <header className="text-white bg-accent-800 px-4 min-h-[5rem] flex flex-col justify-center items-center">
+    <header className="text-white bg-accent-900 px-4 min-h-[5rem] flex flex-col justify-center items-center">
       {/* <div className="w-full container mx-auto my-4"> */}
       <div className="container w-full px-0 mx-auto my-4 flex flex-col items-center justify-center sm:flex-row sm:justify-between gap-y-7">
         <Logo />
