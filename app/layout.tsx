06187279712ce8d4149635574bb9b105c4d7633c
KatoTimothy <katotimuci@gmail.com>
2024-02-28 15:17:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className={`${inter.variable} font-sans antialiased bg-cream-200 `}>
         <div className=" ">
           <Header />
-          <div className="max-w-5xl mx-auto px-4 ">{children}</div>
+          <div className="max-w-7xl mx-auto px-4 ">{children}</div>
         </div>
       </body>
     </html>
