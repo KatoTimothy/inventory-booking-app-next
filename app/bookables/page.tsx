aa63a14cf14043d2b5f2e96f3d4445bfe13f4936
KatoTimothy <katotimuci@gmail.com>
2024-03-06 10:55:02 +0300

    Added page's search params type object

--- app/bookables/page.tsx
+++ app/bookables/page.tsx
@@ -1,24 +1,24 @@
-import React, { Suspense } from "react";
-import BookablesList from "../ui/bookables/bookables-list";
-import { getAllBookables } from "@/app/lib/api";
+import { getAllBookables, findBookableById } from "@/app/lib/api";
 import BookableDetails from "../ui/bookables/bookable-details";
+import BookablesList from "../ui/bookables/bookables-list";
 
-async function BookablesPage({
-  searchParams,
-}: {
-  searchParams: { bookableId: string };
-}) {
-  const bookableId = parseInt(searchParams.bookableId, 10);
+async function BookablesPage() {
+  //fetch page data
   const bookables = await getAllBookables();
-
-  const bookable = bookables.find((b) => b.id === bookableId) || bookables[0];
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-[minmax(0,1fr),minmax(0,3fr)] gap-8 mt-10">
       <BookablesList bookables={bookables} />
-      <BookableDetails bookable={bookable} />
+      <BookableDetails bookables={bookables} />
     </div>
   );
 }
+type BookablesPageSearchParams = {
+  searchParams: {
+    bookableId: string;
+    date_lte: string;
+    date_gte: string;
+  };
+};
 
 export default BookablesPage;
