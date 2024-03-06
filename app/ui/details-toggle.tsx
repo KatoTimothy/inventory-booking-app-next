259e4313c9f1df675dae51727cea58f1e75c8118
KatoTimothy <katotimuci@gmail.com>
2024-03-06 13:01:25 +0300

    Delete details-toggle.tsx

--- app/ui/details-toggle.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-function DetailsToggle() {
-const [toggleDetails, setToggleDetails] = useState(true)
-  return (
-    
-  );
-}
-
-export default DetailsToggle;
