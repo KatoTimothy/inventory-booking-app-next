8b8f8a366886a8a9e9f6a5d8e396c9220e576f59
KatoTimothy <katotimuci@gmail.com>
2024-03-06 12:50:47 +0300

    Update page.tsx

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -2,31 +2,25 @@ import React from "react";
 import { getAllBookables, getAllBookings } from "../lib/api";
 import BookablesList from "../ui/bookables/bookables-list";
 import WeekPicker from "../ui/bookings/week-picker";
-import BookingsGridWrapper from "../ui/bookings/booking-grid-wrapper";
+import BookingsGrid from "../ui/bookings/bookings-grid";
 
-async function BookingsPage({
-  searchParams,
-}: {
-  searchParams: { bookableId: string; date_gte: string };
-}) {
+async function BookingsPage() {
   //fetch data
   const [bookings, bookables] = await Promise.all([
     getAllBookings(),
     getAllBookables(),
   ]);
 
-  //read params
-  const bookableId = parseInt(searchParams.bookableId, 10);
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-[minmax(0,1fr),minmax(0,3fr)] gap-8 justify-between mt-10">
       <BookablesList bookables={bookables} />
       <div className="bookings-section">
         <WeekPicker />
-        <BookingsGridWrapper bookings={bookings} bookables={bookables} />
+        <div className="bookings-grid">
+          <BookingsGrid bookings={bookings} bookables={bookables} />
+        </div>
       </div>
     </div>
   );
 }
-
 export default BookingsPage;
